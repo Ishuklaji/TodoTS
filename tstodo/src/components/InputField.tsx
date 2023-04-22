3df8512620cc5f06cd41469f20400b061ee29045
Ishuklaji <ishuklaji100@gmail.com>
2023-04-23 02:17:46 +0530

    Todo app completed

--- tstodo/src/components/InputField.tsx
+++ tstodo/src/components/InputField.tsx
@@ -1,19 +1,35 @@
-import React from 'react'
-import "./styles.css"
+import React, { useRef } from "react";
+import "./styles.css";
 
-interface Props{
-    todo:string,
-    setTodo:React.Dispatch<React.SetStateAction<string>>
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (e: React.FormEvent) => void;
 }
 
-const InputField = ({todo,setTodo}:Props) => {
-    
+const InputField = ({ todo, setTodo, handleAdd }: Props) => {
+  const inputRef = useRef<HTMLInputElement>(null);
   return (
-    <form className="input">
-     <input type="text" value={todo} onChange={(e)=>setTodo(e.target.value)}placeholder="Enter a task" className="inputBox"/>
-     <button className='inputSubmit' type ="submit">Go</button>
+    <form
+      className="input"
+      onSubmit={(e) => {
+        handleAdd(e);
+        inputRef.current?.blur();
+      }}
+    >
+      <input
+        ref={inputRef}
+        type="text"
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+        placeholder="Enter a task"
+        className="inputBox"
+      />
+      <button className="inputSubmit" type="submit">
+        Go
+      </button>
     </form>
-  )
-}
+  );
+};
 
-export default InputField
+export default InputField;
---
+++ tstodo/src/components/TodoList.tsx
@@ -0,0 +1,69 @@
+import React from "react";
+import { Todo } from "../model";
+import SingleTodo from "./SingleTodo";
+import { Droppable } from "react-beautiful-dnd";
+
+interface props {
+  todos: Array<Todo>;
+  setTodos: React.Dispatch<React.SetStateAction<Array<Todo>>>;
+  setCompletedTodos: React.Dispatch<React.SetStateAction<Array<Todo>>>;
+  completedTodos: Array<Todo>;
+}
+
+const TodoList: React.FC<props> = ({
+  todos,
+  setTodos,
+  completedTodos,
+  setCompletedTodos,
+}) => {
+  return (
+    <div className="container">
+      <Droppable droppableId="TodosList">
+        {(provided, snapshot) => (
+          <div
+            className={`todos ${snapshot.isDraggingOver ? "dragactive" : ""}`}
+            ref={provided.innerRef}
+            {...provided.droppableProps}
+          >
+            <span className="todosHeading">Active Tasks</span>
+            {todos?.map((todo, index) => (
+              <SingleTodo
+                index={index}
+                todos={todos}
+                todo={todo}
+                key={todo.id}
+                setTodos={setTodos}
+              />
+            ))}
+            {provided.placeholder}
+          </div>
+        )}
+      </Droppable>
+      <Droppable droppableId="TodosRemove">
+        {(provided, snapshot) => (
+          <div
+            ref={provided.innerRef}
+            {...provided.droppableProps}
+            className={`todos  ${
+              snapshot.isDraggingOver ? "dragcomplete" : "remove"
+            }`}
+          >
+            <span className="todosHeading">Completed Tasks</span>
+            {completedTodos?.map((todo, index) => (
+              <SingleTodo
+                index={index}
+                todos={completedTodos}
+                todo={todo}
+                key={todo.id}
+                setTodos={setCompletedTodos}
+              />
+            ))}
+            {provided.placeholder}
+          </div>
+        )}
+      </Droppable>
+    </div>
+  );
+};
+
+export default TodoList;
